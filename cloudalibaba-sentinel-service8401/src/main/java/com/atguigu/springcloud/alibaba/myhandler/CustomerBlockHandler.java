b6ec9e8fff2928dc9b23ded5d8ee35487d9c9a7d
Stanley <819605267@qq.com>
2020-05-23 19:41:20 +0800

    新增Sentinel

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/myhandler/CustomerBlockHandler.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/myhandler/CustomerBlockHandler.java
@@ -5,13 +5,13 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 
 
-//public class CustomerBlockHandler {
-//
-//    public static CommonResult handlerException(BlockException exception) {
-//        return new CommonResult(4444,"按客戶自定义,global handlerException----1");
-//    }
-//
-//    public static CommonResult handlerException2(BlockException exception) {
-//        return new CommonResult(4444,"按客戶自定义,global handlerException----2");
-//    }
-//}
+public class CustomerBlockHandler {
+
+    public static CommonResult handlerException(BlockException exception) {
+        return new CommonResult(4444,"按客戶自定义,global handlerException----1");
+    }
+
+    public static CommonResult handlerException2(BlockException exception) {
+        return new CommonResult(4444,"按客戶自定义,global handlerException----2");
+    }
+}
