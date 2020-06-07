0ed273797129bc161be841149b6b641090a69376
Stanley <819605267@qq.com>
2020-06-08 00:46:54 +0800

    seata 分布式事务练习

---
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/dao/OrderDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud.alibaba.dao;
+
+import com.atguigu.springcloud.alibaba.domain.Order;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface OrderDao {
+    //1 新建订单
+    void create(Order order);
+
+    //2 修改订单状态，从零改为1
+    void update(@Param("userId") Long userId, @Param("status") Integer status);
+
+    //3 查询记录总数保证id连续
+    Integer countRecord();
+}
